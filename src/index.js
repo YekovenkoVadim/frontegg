76301fd38ce7e54beaa24e14bc5bdd83569264ac
YekovenkoVadim <yekovenko.vadim@gmail.com>
2024-01-01 20:06:37 +0200

    Frontegg integration and PW installed

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+    baseUrl: 'https://auth.loudapi.com',
+    clientId: '93447df4-edcc-45e5-8664-9fb8c196cf44'
+};
+
+ReactDOM.render(
+    <FronteggProvider contextOptions={contextOptions} tracing={true} hostedLoginBox={true} authOptions={{ keepSessionAlive: true }}>
+        <App />
+    </FronteggProvider>,
+    document.getElementById('root')
+);
