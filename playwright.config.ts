93ebfa400ccc1edd40297ac13e74a7d056980b15
YekovenkoVadim <yekovenko.vadim@gmail.com>
2024-01-16 01:34:36 +0200

    yml file updated with deploy app removed

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     baseURL: 'http://localhost:3000/',
+    // baseURL: 'https://app-cmo8zo3vh6q4.frontegg.com/oauth/account/login',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
