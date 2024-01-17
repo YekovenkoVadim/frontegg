bc56868edd91cab3dd47732f6b4bcd148e633061
YekovenkoVadim <yekovenko.vadim@gmail.com>
2024-01-17 19:26:02 +0200

    @ertuyf reporter only

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,9 +16,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: 'html',
   // reporter: 'allure-playwright',
@@ -27,7 +27,8 @@ export default defineConfig({
   //   ['allure-playwright']
   // ],
   reporter: [
-    ['list'],
+    // ['html'],
+    // ['list'],
     ['@estruyf/github-actions-reporter', {
       title: 'Detailed test results:',
       useDetails: true,
