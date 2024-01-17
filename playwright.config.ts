24d620674f14db7dc9bdd66d63267ffdb734fb15
YekovenkoVadim <yekovenko.vadim@gmail.com>
2024-01-17 17:12:37 +0200

    yml - @estruyf/github-actions-reporter added

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,12 +20,21 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  // reporter: 'html',
   // reporter: 'allure-playwright',
   // reporter: [
   //   ['line'], 
   //   ['allure-playwright']
   // ],
+  reporter: [
+    ['html'],
+    ['list'],
+    ['@estruyf/github-actions-reporter', {
+      title: 'Detailed test results:',
+      useDetails: true,
+      showError: true
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -35,6 +44,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: process.env.VIDEO ? 'on' : 'off'
   },
 
   /* Configure projects for major browsers */
